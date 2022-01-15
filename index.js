7e850c135e89f28c7892a8e931c2ba7d7426d6a0
Francesco <goras.francesco@gmail.com>
2022-01-15 17:14:58 +1300

    Simple express server with posts and post/:title endpoints

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import { postsContent } from './dataLoader.js'
+import { articles } from './articleinfo.js'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 6969
+const app = express()
+
+//middleware
+app.use(bodyParser.json())
+app.use(cors())
+
+// routes
+app.get('/posts', (req, res) => {
+    res.status(200).send({
+        posts: articles,
+    })
+})
+app.get('/post/:title', (req, res) => {
+    const { title } = req.params
+    console.log(postsContent)
+    res.status(200).send({
+        postData: postsContent[title]
+    })
+})
+
+//listen
+app.listen(PORT, () => {
+    console.info(`FoxTrot Server listening on ${PORT}`)
+})
